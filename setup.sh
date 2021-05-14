2a4238abf0300975b263a0f3e9a66b5c07dfa2e4
dnnnp <daniel@daneug.de>
2021-05-14 11:57:14 +0200

    Update setup.sh
    added snap packages

--- setup.sh
+++ setup.sh
@@ -5,3 +5,24 @@ sudo apt install python3 python3-pip golang snapd build-essential python3-dev -y
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python3 1
 
 python -m pip install ansible hcloud
+
+# install snap packages
+sudo snap install android-studio --classic
+sudo snap install barrier
+sudo snap install beekeeper-studio
+sudo snap install chromium
+sudo snap install code --classic
+sudo snap install flameshot
+sudo snap install gimp
+sudo snap install go --classic
+sudo snap install inkscape
+sudo snap install insomnia
+sudo snap install intellij-idea-community --classic
+sudo snap install kotlin --classic
+sudo snap install libreoffice
+sudo snap install lxd
+sudo snap install multipass
+sudo snap install netbeans --classic
+sudo snap install powershell --classic
+sudo snap install pycharm-community --classic
+sudo snap install thunderbird

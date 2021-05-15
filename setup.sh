ba3ae60115b609bd2b1c260eb96669e1d2cc7db1
dnnnp <daniel@daneug.de>
2021-05-15 06:25:17 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 sudo apt update && sudo apt dist-upgrade -y
-sudo apt install python3 python3-pip golang snapd build-essential python3-dev -y
+sudo apt install python3 python3-pip golang snapd build-essential python3-dev guake -y
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python3 1
 
+python -m pip install -U pip
 python -m pip install ansible hcloud
 
 # install snap packages
